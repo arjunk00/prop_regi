bcdfbdb29093144d5f1c8a6b80d0c7318b1b436a
Deepu Yadav <deepu.ug20@nsut.ac.in>
2022-04-09 17:29:00 +0530

    about us section added with logo

--- static/js_files/index.js
+++ static/js_files/index.js
@@ -4,3 +4,26 @@ const links = document.querySelector(".links");
 navToggle.addEventListener("click", () => {
     links.classList.toggle("show-links");
 });
+
+// about us -------------------------------------------------------------------------------------------------
+
+
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
