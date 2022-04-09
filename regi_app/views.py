c8675f88e335aa31100c252bf27ba3e9725389fc
Arjun Kulkarni <arjunkulkarni23@protonmail.com>
2022-04-09 18:04:15 +0530

    try web3 connection

--- regi_app/views.py
+++ regi_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from web3 import Web3
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 # Create your views here.
 
@@ -6,4 +8,11 @@ from django.conf import settings
 def index(request):
     return render(request, 'index.html')
 
-
+def blockchain(request):
+    ganache_url="http://127.0.0.1:7545"
+    web3=Web3(Web3.HTTPProvider(ganache_url))
+    success=web3.isConnected()
+    if success==1:
+        return redirect('/')
+    else:
+        return redirect('/blockchain')
